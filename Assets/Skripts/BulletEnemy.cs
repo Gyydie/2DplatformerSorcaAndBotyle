4ae6f33d09cf0d22974989dee2c9c62f73f1d989
Denis Arkhipov <denarxipow@yandex.ru>
2021-01-24 22:04:55 +0300

    FireEnemy
    FireEnemy

---
+++ Assets/Skripts/BulletEnemy.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+
+public class BulletEnemy : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private float _speed = 3.0f;
+    [SerializeField] private float _lifeTime = 4.0f;
+    [SerializeField] private float _damage = 0.2f;
+
+    Vector3 Dir = new Vector3(0, 0, 0);
+
+    #endregion
+
+
+    #region UnityMethods
+
+    void Start()
+    {
+        Dir.x = _speed;
+        Destroy(gameObject, _lifeTime);
+    }
+
+    private void FixedUpdate()
+    {
+        gameObject.transform.position += -transform.right * _speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            var player = collision.gameObject.GetComponent<HealthBar>();
+            player.HurtPlayer(_damage);
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+}
--- Assets/Skripts/EnemyController.cs
+++ Assets/Skripts/EnemyController.cs
@@ -9,13 +9,18 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private LayerMask _playerLauerMask;
     [SerializeField] private float _speed;
     [SerializeField] private float _damage = 0.2f;
+    [SerializeField] private Transform _startBullet;
+    [SerializeField] private GameObject _bullet;
     private Vector3 _moveDirection = Vector3.zero;
     private RaycastHit2D _checkPlayerLeft;
     private RaycastHit2D _checkPlayerRight;
-
+    private Vector3 _moveDirectionY;
 
     private bool _isPlayerLeft;
     private bool _isPlayerRight;
+    private float nextTime = 0.0F;
+    private float timeRate = 1F;
+
 
     #endregion
 
@@ -24,19 +29,29 @@ public class EnemyController : MonoBehaviour
 
     void Update()
     {
+
         TrggerPlayer();
 
-        if(_isPlayerLeft)
+        if (_isPlayerLeft)
         {
             _moveDirection.x = -1;
-        }else if (_isPlayerRight)
+            _moveDirectionY.y = 0;
+            transform.rotation = Quaternion.Euler(_moveDirectionY);
+            Fire();
+        }
+        else if (_isPlayerRight)
         {
             _moveDirection.x = 1;
-        }else
+            _moveDirectionY.y = 180;
+            transform.rotation = Quaternion.Euler(_moveDirectionY);
+            Fire();
+        }
+        else
         {
             _moveDirection.x = 0;
         }
         transform.position += _moveDirection * _speed * Time.deltaTime;
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -52,6 +67,15 @@ public class EnemyController : MonoBehaviour
 
 
     #region Methods
+
+    private void Fire()
+    {
+        if (Time.time > nextTime)
+        {
+            nextTime = Time.time + timeRate;
+            Instantiate(_bullet, _startBullet.position, _startBullet.rotation);
+        }
+    }
 
     void TrggerPlayer()
     {
